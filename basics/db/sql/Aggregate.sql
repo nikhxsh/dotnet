5912911500492b79b210e3cadcf40ad6e359adc6
Nikhilesh <32759036+nikhxsh@users.noreply.github.com>
2025-07-16 23:58:40 +0530

    Update Aggregate.sql

--- basics/db/sql/Aggregate.sql
+++ basics/db/sql/Aggregate.sql
@@ -24,19 +24,70 @@ GROUP BY A.Age
 HAVING A.Age < 40
 ORDER BY TotalLoanAmount DESC
 
--- You can use the PIVOT and UNPIVOT relational operators to change a table-valued expression into another table. 
--- PIVOT rotates a table-valued expression by turning the unique values from one column in the expression into multiple columns in the output. 
--- And PIVOT runs aggregations where they're required on any remaining column values that are wanted in the final output. 
--- UNPIVOT carries out the opposite operation to PIVOT by rotating columns of a table-valued expression into column values.
-SELECT 'Average Balance' AS Average_Balance, [CO], [IN], [IT], [SE], [UK], [USA], [PK]
-FROM(
-	SELECT Country, Balance
-	FROM ACCOUNT
+/* 
+- You can use the PIVOT and UNPIVOT relational operators to change a table-valued expression into another table. 
+- PIVOT rotates a table-valued expression by turning the unique values from one column in the expression into multiple columns in the output. 
+  and PIVOT runs aggregations where they're required on any remaining column values that are wanted in the final output. 
+- UNPIVOT carries out the opposite operation to PIVOT by rotating columns of a table-valued expression into column values.
+Eg: SalesData
++---------+---------+-------+
+| Country | Quarter | Sales |
++---------+---------+-------+
+| USA     | Q1      | 1000  |
+| USA     | Q2      | 1100  |
+| UK      | Q1      | 900   |
+| UK      | Q2      | 950   |
+| IN      | Q1      | 800   |
+| IN      | Q2      | 850   |
++---------+---------+-------+
+*/
+	
+SELECT Country, [Q1], [Q2]
+FROM (
+    SELECT Country, Quarter, Sales
+    FROM SalesData
 ) AS SourceTable
-PIVOT
-(
-	AVG(Balance)
-	FOR Country IN ([CO], [IN], [IT], [SE], [UK], [USA], [PK])
-) AS PivotTable
+PIVOT (
+    SUM(Sales)
+    FOR Quarter IN ([Q1], [Q2])
+) AS PivotedTable;
 
+/* 
+Output:
++---------+------+------+
+| Country | Q1   | Q2   |
++---------+------+------+
+| USA     | 1000 | 1100 |
+| UK      | 900  | 950  |
+| IN      | 800  | 850  |
++---------+------+------+
+*/
 
+SELECT Country, Quarter, Sales
+FROM (
+    SELECT Country, [Q1], [Q2]
+    FROM (
+        SELECT Country, Quarter, Sales
+        FROM SalesData
+    ) AS SourceTable
+    PIVOT (
+        SUM(Sales)
+        FOR Quarter IN ([Q1], [Q2])
+    ) AS PivotedTable
+) AS PivotResult
+UNPIVOT (
+    Sales FOR Quarter IN ([Q1], [Q2])
+) AS UnpivotedTable;
+
+/* 
++---------+---------+-------+
+| Country | Quarter | Sales |
++---------+---------+-------+
+| USA     | Q1      | 1000  |
+| USA     | Q2      | 1100  |
+| UK      | Q1      | 900   |
+| UK      | Q2      | 950   |
+| IN      | Q1      | 800   |
+| IN      | Q2      | 850   |
++---------+---------+-------+
+*/
